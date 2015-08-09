13886bff91b0c53b90521fa4ac7a30f4f603371b
Dan Allen <dan.j.allen@gmail.com>
2015-08-09 00:55:17 -0600

    resolves #4 toggle fullscreen when f key is pressed
    - toggle fullscreen when f key is pressed
      * see https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/Using_full_screen_mode
    - use else if when processing key press

--- dist/bespoke-keys.js
+++ dist/bespoke-keys.js
@@ -1,5 +1,5 @@
 /*!
- * bespoke-keys v1.0.0
+ * bespoke-keys v1.1.0
  *
  * Copyright 2015, Mark Dalgleish
  * This content is released under the MIT license
@@ -9,7 +9,23 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self);var o=n;o=o.bespoke||(o.bespoke={}),o=o.plugins||(o.plugins={}),o.keys=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 module.exports = function(options) {
   return function(deck) {
-    var isHorizontal = options !== 'vertical';
+    var isHorizontal = options !== 'vertical',
+      fullScreenEnabled = document.fullscreenEnabled || document.webkitFullscreenEnabled ||
+          document.mozFullScreenEnabled || document.msFullscreenEnabled,
+      toggleUserFullScreen = function() {
+        var element, method;
+        // NOTE does not exit from full screen initiated by F11
+        if (document.fullscreenElement || document.mozFullScreenElement ||
+            document.webkitFullscreenElement || document.msFullscreenElement) {
+          method = (element = document).exitFullscreen || element.webkitExitFullscreen ||
+              element.mozCancelFullScreen || element.msExitFullscreen;
+        }
+        else {
+          method = (element = document.documentElement).requestFullscreen || element.webkitRequestFullscreen ||
+              element.mozRequestFullScreen || element.msRequestFullscreen;
+        }
+        if (method) { method.apply(element); }
+      };
 
     document.addEventListener('keydown', function(e) {
       if (e.which == 34 || // PAGE DOWN
@@ -17,12 +33,14 @@ module.exports = function(options) {
         (isHorizontal && e.which == 39) || // RIGHT
         (!isHorizontal && e.which == 40) // DOWN
       ) { deck.next(); }
-
-      if (e.which == 33 || // PAGE UP
+      else if (e.which == 33 || // PAGE UP
         (e.which == 32 && e.shiftKey) || // SPACE + SHIFT
         (isHorizontal && e.which == 37) || // LEFT
         (!isHorizontal && e.which == 38) // UP
       ) { deck.prev(); }
+      else if (e.which == 70) {
+        if (fullScreenEnabled) toggleUserFullScreen();
+      }
     });
   };
 };
